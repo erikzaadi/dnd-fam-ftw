970ef3b9004af7beb97ac11d32d2a2507a4b82d4
erikzaadi <erik.zaadi@gmail.com>
2026-09-24 00:20:01 +0300

    MOAR loading puns!

--- frontend/src/components/InstantStartLoader.tsx
+++ frontend/src/components/InstantStartLoader.tsx
@@ -14,12 +14,48 @@ const PUNS = [
   'The prophecy is being speed-read...',
   'Someone forgot to bring torches. Again.',
   'Debating whether goblins count as a warm-up...',
+  'Asking the skeleton if it has a bone to pick...',
+  'Convincing the bard this is not a solo campaign...',
+  'Putting the romance in necromancer...',
+  'Teaching the gelatinous cube to think outside the box...',
+  'Reminding the rogue that sharing is not a sleight of hand...',
+  'Waiting for the wizard to finish a spell check...',
+  'Telling the paladin to lighten up. Literally.',
+  'Helping the druid get to the root of the problem...',
+  'Asking the barbarian to use their indoor roar...',
+  'Checking the chest for teeth...',
+  'Checking the teeth for treasure...',
+  'Explaining to the mimic that this is a closed-door meeting...',
+  'Giving the dragon a stern hoarding...',
+  'Making sure the lich has a life outside work...',
+  'Asking the cleric for a little divine inter-venting...',
+  'Rolling for initiative. Losing to the furniture.',
+  'Debating whether a fireball counts as a doorbell...',
+  'Filing a noise complaint against the thunder spell...',
+  'Trying to get the party on the same side quest...',
+  'Arguing about who gets the slightly worse sword...',
+  'Convincing the fighter that every problem is not a nail...',
+  'Asking the warlock to read the terms and conditions...',
+  'Giving the ranger some space. About 120 feet.',
+  'Untangling the plot hooks from the fishing gear...',
+  'Taking the dungeon decor for granite...',
+  'Asking the golem to be a little boulder...',
+  'Making the troll pay the bridge toll. How the turns table.',
+  'Putting the loot in absolutely not a mimic...',
+  'Checking whether emotional baggage fits in a bag of holding...',
+  'Persuading the dice that one is not the only number...',
+  'Rehearsing a heroic entrance. Tripping on the cape.',
+  'Explaining that stealth is not yelling quietly...',
+  'Waiting for the monk to punch in...',
+  'Taking a short rest from planning the long rest...',
+  'Asking the beholder to keep an eye out. Just one.',
+  'Finding out who used the quest map as a napkin...',
 ];
 
 const CYCLE_MS = 4500;
 
 export const InstantStartLoader = () => {
-  const [punIndex, setPunIndex] = useState(0);
+  const [punIndex, setPunIndex] = useState(() => Math.floor(Math.random() * PUNS.length));
 
   useEffect(() => {
     const interval = setInterval(() => {
